42dee90c551e3f1ac93dd10846a23247a8b16c1b
cosmin_louis <32954953+cosmineala@users.noreply.github.com>
2018-05-15 15:40:51 +0300

    crypting_2.1

--- main.c
+++ main.c
@@ -1,11 +1,37 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "crypting"
 
 
 
 int int main()
 {
-	
+	int meniu;
 
-	
+	printf("\nINSERT: \n1 to encrypt \n2 to decrypt \n>>>");
+
+	scanf("%d",&meniu);
+
+	switch(meniu){
+
+		case 1:
+			printf("\n");
+			crip_cr();
+			break;
+
+		case 2:
+			printf("\n");
+			crip_de();
+			break;
+
+		default :
+			printf("Incorect insertion please falow the instruction");
+			main();
+			break;
+	}
+
+
 	return 0;
 }
